64ad40d8b7226835cad44e644e4a97618d9b6ec9
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-01 18:53:45 +0530

    Alphabet Vowel or Consonant

---
+++ VowelConsonantPrograming/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace VowelConsonantPrograming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VowelOrConsonant vowelOrConsonant=new VowelOrConsonant();
+            vowelOrConsonant.Display();
+
+        }
+    }
+}
+
---
+++ VowelConsonantPrograming/VowelOrConsonant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelConsonantPrograming
+{
+    public class VowelOrConsonant
+    {
+        public void Display()
+        {
+            Console.WriteLine("Enter the any character");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + "is vowel");
+            }
+            else
+            {
+                Console.WriteLine(ch + "is consonant ");
+            }
+
+
+        }
+    }
+}
+    
