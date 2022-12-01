54e94c80a03a3ee1517da8f75c44e50f81a11355
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-01 17:52:22 +0530

    Print Prime Factor

---
+++ FactorPrograming/FactorialNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorPrograming
+{
+    public class FactorialNumber
+    {
+        public void CheckFactor()
+        {
+            int fact = 1;
+
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of" + num + "is" + fact);
+        }
+    }
+}
+
+    
+
---
+++ FactorPrograming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FactorPrograming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialNumber factorialNumber=new FactorialNumber();
+            factorialNumber.CheckFactor();
+        }
+    }
+}
