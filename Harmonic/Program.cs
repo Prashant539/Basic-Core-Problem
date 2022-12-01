46b681040d7e240499e583cd2a7b21f722cf94e7
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-01 17:33:27 +0530

    Print Harmonic Number

---
+++ Harmonic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Harmonic
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            HarmonicNumber harmonicNumber = new HarmonicNumber();
+            harmonicNumber.Equals(harmonicNumber);
+            Console.WriteLine();
+        }
+       
+    }
+}
