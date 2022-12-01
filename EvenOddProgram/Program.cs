9d37e188eeb72d894e2ba5f9b51dce1bec05cd15
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-01 18:40:26 +0530

    Number is Even or Odd

---
+++ EvenOddProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EvenOddProgram
+{
+    internal class Program
+    {
+     static void Main(string[] args)
+        {
+            EvenOrOdd evenOrOdd = new EvenOrOdd();
+            evenOrOdd.Dispaly();
+
+        }
+    }
+}
